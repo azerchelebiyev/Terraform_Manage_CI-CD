781a6c652e1f6d1ab01ba1f2085c915b0d0fbfcc
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-09 15:20:45 +0400

    Update main.tf

--- modules/elasticsearch/main.tf
+++ modules/elasticsearch/main.tf
@@ -1,1 +1,15 @@
+resource "aws_elasticsearch_domain" "this" {
+  domain_name           = var.domain_name
+  elasticsearch_version = "7.10"
+
+  cluster_config {
+    instance_type  = "t3.small.elasticsearch"
+    instance_count = 1
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+}
 
