7ec009b674ea3d9dc74393e3d16ed61d8d80b9ce
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-09 16:55:47 +0400

    Update terraform_config.tf

--- envs/prod/terraform_config.tf
+++ envs/prod/terraform_config.tf
@@ -1,23 +1,5 @@
-locals {
-  env = "prod"
-}
-
-variable "KKE_ENV" {
-  default = local.env
-}
-
-variable "KKE_DYNAMODB_TABLE_NAME" {
-  default = "nautilus-${local.env}-table"
-}
-
-variable "KKE_SECRET_NAME" {
-  default = "nautilus-${local.env}-secret"
-}
-
-variable "KKE_SECRET_VALUE" {
-  default = "nautilus-${local.env}-value"
-}
-
-variable "KKE_ELASTICSEARCH_DOMAIN" {
-  default = "nautilus-${local.env}-es"
-}
+variable "KKE_ENV" { default = "prod" }
+variable "KKE_DYNAMODB_TABLE_NAME" { default = "nautilus-prod-table" }
+variable "KKE_SECRET_NAME" { default = "nautilus-prod-secret" }
+variable "KKE_SECRET_VALUE" { default = "nautilus-prod-value" }
+variable "KKE_ELASTICSEARCH_DOMAIN" { default = "nautilus-prod-es" }
