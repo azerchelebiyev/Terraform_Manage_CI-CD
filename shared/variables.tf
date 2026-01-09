892768c192778767d4e54ba6bfed43fab31ffc85
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-09 16:54:43 +0400

    Update variables.tf

--- shared/variables.tf
+++ shared/variables.tf
@@ -1,25 +1,5 @@
-variable "KKE_ENV" {
-  description = "Environment name (dev or prod)"
-  type        = string
-}
-
-variable "KKE_DYNAMODB_TABLE_NAME" {
-  description = "DynamoDB table name"
-  type        = string
-}
-
-variable "KKE_SECRET_NAME" {
-  description = "Secrets Manager secret name"
-  type        = string
-}
-
-variable "KKE_SECRET_VALUE" {
-  description = "Secret value"
-  type        = string
-  sensitive   = true
-}
-
-variable "KKE_ELASTICSEARCH_DOMAIN" {
-  description = "Elasticsearch domain name"
-  type        = string
-}
+variable "KKE_ENV" {}
+variable "KKE_DYNAMODB_TABLE_NAME" {}
+variable "KKE_SECRET_NAME" {}
+variable "KKE_SECRET_VALUE" { sensitive = true }
+variable "KKE_ELASTICSEARCH_DOMAIN" {}
