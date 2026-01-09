ae28c7c870c0ccb17a58cb608e936ae60bb0e810
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-09 15:22:56 +0400

    Update outputs.tf

--- shared/outputs.tf
+++ shared/outputs.tf
@@ -1,1 +1,12 @@
+output "Kke_dynamodb_table_name" {
+  value = module.dynamodb.table_name
+}
+
+output "kke_secret_arn" {
+  value = module.secretsmanager.secret_arn
+}
+
+output "kke_elasticsearch_domain_endpoint" {
+  value = module.elasticsearch.endpoint
+}
 
