1a67d298b9058e0dce49633cd6c4356d9f41e2e9
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-09 15:59:56 +0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "AWS_REGION" {}
+variable "KKE_ENV" {}
+variable "KKE_DYNAMODB_TABLE_NAME" {}
+variable "KKE_SECRET_NAME" {}
+variable "KKE_SECRET_VALUE" { sensitive = true }
+variable "KKE_ELASTICSEARCH_DOMAIN" {}
