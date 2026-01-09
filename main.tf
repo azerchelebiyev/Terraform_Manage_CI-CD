356bbbd4ae166b768839c5d567ffe881055a7f82
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-09 16:03:11 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-module "dynamodb" {
-  source     = "./modules/dynamodb"
-  table_name = var.KKE_DYNAMODB_TABLE_NAME
-}
-
-module "secretsmanager" {
-  source       = "./modules/secretsmanager"
-  secret_name  = var.KKE_SECRET_NAME
-  secret_value = var.KKE_SECRET_VALUE
-}
-
-module "elasticsearch" {
-  source      = "./modules/elasticsearch"
-  domain_name = var.KKE_ELASTICSEARCH_DOMAIN
+module "env" {
+  source = "./envs/dev"
 }
