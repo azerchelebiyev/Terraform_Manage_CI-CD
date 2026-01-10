91c05d27b63045a3907a0ef29a76439b65965f38
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-11 00:55:04 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,28 @@
 module "dynamodb" {
-  source = "./modules/dynamodb"
-
+  source     = "../modules/dynamodb"
   table_name = var.KKE_DYNAMODB_TABLE_NAME
 }
 
-module "secretsmanager" {
-  source = "./modules/secretsmanager"
-
+module "secrets" {
+  source       = "../modules/secretsmanager"
   secret_name  = var.KKE_SECRET_NAME
   secret_value = var.KKE_SECRET_VALUE
 }
 
 module "elasticsearch" {
-  source = "./modules/elasticsearch"
-
+  source      = "../modules/elasticsearch"
   domain_name = var.KKE_ELASTICSEARCH_DOMAIN
 }
 
+# Tələb olunan Output-lar
 output "Kke_dynamodb_table_name" {
-  value = module.dynamodb.name
+  value = module.dynamodb.table_name
 }
 
 output "kke_secret_arn" {
-  value = module.secretsmanager.arn
+  value = module.secrets.secret_arn
 }
 
 output "kke_elasticsearch_domain_endpoint" {
-  value = module.elasticsearch.endpoint
+  value = module.elasticsearch.domain_endpoint
 }
