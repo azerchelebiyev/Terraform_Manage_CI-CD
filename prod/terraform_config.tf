fc1ae9ecf78d19e2cb46fafcfdcd745a2b6be42b
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-11 00:58:33 +0400

    Update terraform_config.tf

--- prod/terraform_config.tf
+++ prod/terraform_config.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  backend "s3" {
-    bucket         = "nautilus-terraform-states"
-    key            = "prod/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "nautilus-terraform-lock"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
-  default_tags {
-    tags = {
-      Project     = "nautilus"
-      Environment = "prod"
-      Owner       = "kke"
-    }
-  }
+  region = "us-east-1"
 }
