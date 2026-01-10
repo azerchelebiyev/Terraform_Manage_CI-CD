c00ab6bb0aba4f022b7f8a68f63c0631f8d2461a
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-11 00:54:47 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,5 @@
-variable "KKE_ENV" {
-  type = string
-}
-
-variable "KKE_DYNAMODB_TABLE_NAME" {
-  type = string
-}
-
-variable "KKE_SECRET_NAME" {
-  type = string
-}
-
-variable "KKE_SECRET_VALUE" {
-  type = string
-  sensitive = true
-}
-
-variable "KKE_ELASTICSEARCH_DOMAIN" {
-  type = string
-}
+variable "KKE_ENV" { type = string }
+variable "KKE_DYNAMODB_TABLE_NAME" { type = string }
+variable "KKE_SECRET_NAME" { type = string }
+variable "KKE_SECRET_VALUE" { type = string }
+variable "KKE_ELASTICSEARCH_DOMAIN" { type = string }
