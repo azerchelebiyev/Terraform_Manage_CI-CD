181ce25a03d65f552c882204f655efc8b7747eab
azerchelebiyev <34437289+azerchelebiyev@users.noreply.github.com>
2026-01-10 13:17:09 +0400

    Update main.tf

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -7,12 +7,4 @@ resource "aws_dynamodb_table" "this" {
     name = "id"
     type = "S"
   }
-
-  tags = {
-    Environment = var.env
-  }
-}
-
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.this.name
 }
