6841cee1150ed30a81c45a5dffda782011e3acf6
Azer Chalabiyev <azer.chelebiyev@gmail.com>
2026-01-11 02:02:50 +0400

    updated5

--- main.tf
+++ main.tf
@@ -1,23 +1,18 @@
-# 1. DynamoDB Modulu
 module "dynamodb" {
-  source     = "../../modules/dynamodb"
+  source     = "./modules/dynamodb" # Artıq ../.. ehtiyac yoxdur, çünki qovluq linklənib
   table_name = var.KKE_DYNAMODB_TABLE_NAME
 }
 
-# 2. Secrets Manager Modulu
 module "secretsmanager" {
-  source       = "../../modules/secretsmanager"
+  source       = "./modules/secretsmanager"
   secret_name  = var.KKE_SECRET_NAME
   secret_value = var.KKE_SECRET_VALUE
 }
 
-# 3. Elasticsearch Modulu
 module "elasticsearch" {
-  source      = "../../modules/elasticsearch"
+  source      = "./modules/elasticsearch"
   domain_name = var.KKE_ELASTICSEARCH_DOMAIN
 }
-
-# --- Tələb Olunan Output-lar ---
 
 output "Kke_dynamodb_table_name" {
   value       = module.dynamodb.table_name
